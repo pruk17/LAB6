0835f47bdbb8b852b4c7a01834cfad56c92ce7a8
Chaiyapruk Parinyanuntakan <chaiyapruk6494@gmail.com>
2023-12-19 16:55:01 +0700

    adjust lab6

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,9 +1,22 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
 
 int main(){
+    int num; 
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> num; 
+    int even = 0, odd = 0; 
+    while(num != 0){ 
+        if(num%2 == 0){ 
+            even++;  
+        }else{
+            odd++;  
+        }
+        cout << "Enter an integer: ";
+        cin >> num; 
+    }
+    cout << "#Even numbers = " << even << endl; 
+    cout << "#Odd numbers = " << odd;  
     return 0;
 }
